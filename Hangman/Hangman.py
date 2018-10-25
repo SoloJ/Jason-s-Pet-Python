7bea18f7512686ceb618a9038670849df194ede1
Jason Solomon <jsolo@outlook.com>
2018-10-25 09:45:23 -0700

    Path change for hangman

--- Hangman/Hangman.py
+++ Hangman/Hangman.py
@@ -1,7 +1,7 @@
 import random
 
 def word_choser():
-    word_list = open("/Users/jasonsolomon/PycharmProjects/Jason_Lessons/venv/Hangman/words.txt")
+    word_list = open("/Users/jasonsolomon/PycharmProjects/Jason-s-Pet-Python/Hangman/Hangman.py")
     contents_of_file = word_list.read()
     word_list.close()
     words = contents_of_file.split()
