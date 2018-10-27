9301d777311b9a550bdc4f700f23d4a5ddfc1383
Jason Solomon <jsolo@outlook.com>
2018-10-27 13:53:37 -0700

    Added A Class

--- Hangman/Hangman.py
+++ Hangman/Hangman.py
@@ -7,66 +7,10 @@ def word_select():
     contents_of_file = word_list.read()
     word_list.close()
     words = contents_of_file.split()
-    global chosen_word
-    global chosen_word_length
-    global blank_display_word
-    blank_display_word = list()
     chosen_word = random.choice(words)
-    chosen_word_length = len(chosen_word)
-    n = 0
-
-    while n < chosen_word_length:
-        blank_display_word.insert(n, "-")
-        n=n+1
-
+    answer = letter_check.LetterCheck(chosen_word)
+    answer.blanker()
+    answer.blank_filler()
+    answer.UserInput()
 word_select()
 
-
-def input_guess():
-    guess = input("What is your Guess?..")
-    answer = letter_check.LetterCheck(chosen_word, guess)
-    answer.checker()
-    global display_word
-    display_word = list(chosen_word)
-    print(chosen_word)
-    print(answer.correct)
-    print(answer.guess)
-    found_letter = "false"
-
-
-    if answer.correct == "true":
-        n = 0
-        while found_letter == "false":
-            if answer.guess not in chosen_word[n]:
-                n = n + 1
-            else:
-                found_letter == "true"
-                word_guess_position = n
-                break
-    elif answer.correct == "false":
-        input_guess()
-
-    blank_display_word.pop(n)
-    blank_display_word.insert(n, display_word[n])
-    input_guess()
-
-
-    if "-" not in blank_display_word:
-        print("you did it")
-        exit()
-
-input_guess()
-
-
-
-
-
-
-
-
-
-
-
-
-
-exit()
--- Hangman/letter_check.py
+++ Hangman/letter_check.py
@@ -1,12 +1,42 @@
 
 class LetterCheck():
-    def __init__(self, chosen_word, guessed_letter):
+    def __init__(self, chosen_word):
         self.word = chosen_word
-        self.guess = guessed_letter
+        self.blank_display_word = list()
+        self.chosen_word_length = len(chosen_word)
+        self.guess = input("What is your Guess?..")
+        self.display_word = list(chosen_word)
 
-    def checker(self):
+
+    def UserInput(self):  # requests another guess from the user
+        self.guess = input("What is your Guess?..")
+
+    def blanker(self):  # call once to create a blank array of length equal to the selected word
+
+        n = 0
+        while n < self.chosen_word_length:
+            self.blank_display_word.insert(n, "-")
+            n = n + 1
+
+    def blank_filler(self): # call to fill in the blanked array with the guessed letter
+
+        self.found_letter = "false"
 
         if self.guess in self.word:
             self.correct = "true"
         else:
             self.correct = "false"
+
+
+        if self.correct == "true":
+            n = 0
+            while self.found_letter == "false":
+                if self.guess not in self.word[n]:
+                    n = n + 1
+                else:
+                    self.found_letter == "true"
+                    self.position = n
+                    break
+
+        self.blank_display_word.pop(self.position)
+        self.blank_display_word.insert(n, self.display_word[n])
