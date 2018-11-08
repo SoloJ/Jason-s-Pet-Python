c0ccdd917daa58f0c362251530e60d5a456c7525
Jason Solomon <jsolo@outlook.com>
2018-11-07 21:01:25 -0800

    Added A Class

--- Mastermind/The_Game.py
+++ Mastermind/The_Game.py
@@ -1,11 +1,14 @@
 import game_engine
 
 def gameplay():
-    color_decoder = {"1":"red", "2":"blue", "3":"yellow", "4":"greem", "5":"purple", "6":"black" }
+    color_decoder = {"1":"red", "2":"blue", "3":"yellow", "4":"green", "5":"purple", "6":"black" }
     difficulty = input("Input your level of difficulty...")
-    mastermind = game_engine.GameEngine(difficulty)
+    mastermind = game_engine.GameEngine(difficulty, color_decoder)
     mastermind.SequenceGen()
-    print(6)
+    mastermind.ColorKey()
+    mastermind.UserInput()
+
+    print(3)
 
 
 
--- Mastermind/game_engine.py
+++ Mastermind/game_engine.py
@@ -1,13 +1,22 @@
 import random
 
 class GameEngine():
-    def __init__(self, game_diffuclty):
-        self.difficulty = game_diffuclty
+    def __init__(self, game_difficulty, color_decoder):
+        self.difficulty = game_difficulty
+        self.decoder = color_decoder
 
     def SequenceGen(self):
         self.sequence = []
-        for x in range(0,6):
-            self.sequence.append(random.randint(1, 6))
+        self.sequence_word = []
+        for x in range(0, int(self.difficulty)):
+            self.sequence.append(random.randint(1, int(self.difficulty)))
+        for x in self.sequence:
+            self.sequence_word.append(self.decoder[str(x)])
+
+
+    def ColorKey(self):
+        for x in self.decoder:
+            print(x + " = " + self.decoder[str(x)])
 
     def UserInput(self):
         self.user_input = input("What is your Guess?..")
